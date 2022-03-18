f062323b57544edf54b9aaa309bb78b8c1dc8bd7
alicerichardeau <alicerichardeau@outlook.com>
2022-03-18 14:06:14 +0100

    cc

--- script.js
+++ script.js
@@ -3,18 +3,15 @@
 
 window.onscroll = function() {
 
-    if (window.innerWidth > 990) {
+    if(window.innerWidth > 990) {
 
         if(document.documentElement.scrollTop > 80) {
 
             document.querySelector('#bloc-menu').style.display = "none";
-            document.querySelector('header').style.position = "sticky";
-
         }
         else {
            
             document.querySelector('#bloc-menu').style.display = "flex";
-            document.querySelector('header').style.position = "initial";
         }
     }
 
