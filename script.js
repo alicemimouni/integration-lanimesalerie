422cfbfd262fbff3f689cb2d96dccadf38895bc7
alicerichardeau <alicerichardeau@outlook.com>
2022-03-18 13:54:37 +0100

    change js position header

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ window.onscroll = function() {
 
     if (window.innerWidth > 990) {
 
-        if(document.documentElement.scrollTop > 10) {
+        if(document.documentElement.scrollTop > 80) {
 
             document.querySelector('#bloc-menu').style.display = "none";
+            document.querySelector('header').style.position = "sticky";
+
         }
         else {
            
             document.querySelector('#bloc-menu').style.display = "flex";
+            document.querySelector('header').style.position = "initial";
         }
     }
 
